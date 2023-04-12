7c61db3a99bac5b9dfc5ea144f220a23ea0d6885
Wessel Kronemeijer <wessel32@gmail.com>
2023-04-12 11:53:21 +0200

    Move wow stuff to its own folder

--- src/UserPages/WowCharacterSheet/index.page.tsx
+++
@@ -1,8 +0,0 @@
-import "./Style.scss";
-
-import { register } from "../../ReactFileClient/PageRegistry/Registry";
-import { RegistryTag } from "../../ReactFileClient/PageRegistry/RegistryTag";
-
-register([RegistryTag`WoW Alt Sheet`, "World of Warcraft Character Sheet"], <>
-    Goodbye, world!
-</>);
